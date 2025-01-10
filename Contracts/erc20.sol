961606d7144a202b66e45f949af9cdb8377d68bc
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2025-01-10 16:12:45 +0100

    Create erc20.sol

---
+++ Contracts/erc20.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.8.0) (token/ERC20/ERC20.sol)
+
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+
+interface IOwnable {
+    function owner() external view returns (address);
+}
