f52ffb404e7cb15c59cd8dcdf9b2add10deacc34
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2025-01-10 16:02:26 +0100

    Create Game.sol

---
+++ Contracts/Game.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+// @title GAME token for battledogs Arena
+// https://twitter.com/0xSorcerers | https://github.com/Dark-Viper | https://t.me/Oxsorcerer | https://t.me/battousainakamoto | https://t.me/darcViper
+
+pragma solidity ^0.8.17;
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "abdk-libraries-solidity/ABDKMath64x64.sol";
+import "./ERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
